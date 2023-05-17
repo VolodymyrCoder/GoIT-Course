63369da57f35d356b6d81547f7c4e2986b54c134
VolodymyrCoder <133695023+VolodymyrCoder@users.noreply.github.com>
2023-05-17 19:23:19 +0300

    Lesson 3, part 1.

--- game.py
+++ game.py
@@ -10,10 +10,12 @@ FPS = p.time.Clock()
 HEIGHT = 800
 WIDTH = 1200
 
+FONT = p.font.SysFont('Verdana', 20)
+
 COLOR_WHITE = (255, 255, 255)
 COLOR_BLACK = (0, 0, 0)
-COLOR_BLUE = (0, 0, 255)
-COLOR_GREEN = (0, 255, 0)
+COLOR_RED = (255, 0, 0)
+COLOR_YELLOW = (255, 255, 0)
 
 main_display = p.display.set_mode((WIDTH, HEIGHT))
 
@@ -31,7 +33,7 @@ player_move_right = [1, 0]
 def create_enemy():
     enemy_size = (30, 30)
     enemy = p.Surface(enemy_size)
-    enemy.fill(COLOR_BLUE)
+    enemy.fill(COLOR_RED)
     rect = p.Rect(WIDTH, random.randint(0, HEIGHT), *enemy_size)
     move = [random.randint(-6, -1), 0]
     return {
@@ -50,7 +52,7 @@ enemies = []
 def create_bonus():
     bonus_size = (30, 30)
     bonus = p.Surface(bonus_size)
-    bonus.fill(COLOR_GREEN)
+    bonus.fill(COLOR_YELLOW)
     rect = p.Rect(random.randint(0, WIDTH), 0, *bonus_size)
     move = [0, random.randint(1, 6)]
     return {
@@ -64,6 +66,8 @@ CREATE_BONUS = p.USEREVENT + 2
 p.time.set_timer(CREATE_BONUS, 1500)
 
 bonuses = []
+
+score = 0
 
 playing = True
 
@@ -99,12 +103,20 @@ while playing:
         if enemy['rect'].left < 0:
             enemies.pop(enemies.index(enemy))
 
+        if player_rect.colliderect(enemy['rect']):
+          playing = False
+
     for bonus in bonuses:
         bonus['rect'] = bonus['rect'].move(bonus['move'])
         main_display.blit(bonus['bonus'], bonus['rect'])
         if bonus['rect'].bottom > HEIGHT:
             bonuses.pop(bonuses.index(bonus))
 
+        if player_rect.colliderect(bonus['rect']):
+          score += 1
+          bonuses.pop(bonuses.index(bonus))
+
+    main_display.blit(FONT.render(str(score), True, COLOR_WHITE), (WIDTH - 50, 20))
     main_display.blit(player, player_rect)
 
     p.display.flip()
